0309472645379b9363c9ff34c88e7830055be38b
Miguel Fraga <mapena.16@est.ucab.edu.ve>
2021-04-25 10:34:42 -0400

    Ajuestes generales del inventario

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,14 +9,11 @@ public class GameController : MonoBehaviour
     public InventoryManager inventoryManager;
     private bool openInventory = false;
 
-    public Texture2D cursorTexture;
-
     void Start()
     {
         //Initialize tools
         ToolHelper.Init(toolHelperData);
         inventoryManager.inventoryPanel.gameObject.SetActive(openInventory);
-        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
     }
 
     void Update()
