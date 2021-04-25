248ec1a3c154657a313688c41eafec1174d991d6
Diego Luis Di Salvatore Veiga <disalvatorediego@gmail.com>
2021-04-25 06:19:01 +0200

    [h23] Enemy animations v0.1

--- Assets/Scripts/Models/Enemy.cs
+++ Assets/Scripts/Models/Enemy.cs
@@ -7,14 +7,15 @@ public class Enemy: ScriptableObject
 {
     public int MaxHealth = 5;
 
+    [Header("Graphic Configs")]
+    public Sprite sprite;
+    public RuntimeAnimatorController animatorController;
+
     [Header("Enemy Behaviour Configs")]
-    public Color32 IdleColor = new Color32(255,255,255,255);
     public float IdleSpeed = 50.0f;
     public float IdleTimeBetweenSwams = 1.5f;
-    public Color32 FollowPlayerColor = new Color32(255,100,0,255);
     public float FollowPlayerSpeed = 75.0f;
     public float FollowPlayerTimeBetweenSwams = 0.1f;
-    public Color32 EscapeColor = new Color32(0,0,255,255);
     public float EscapeSpeed = 100.0f;
     public float EscapeTimeBetweenSwams = 0.1f;
 
